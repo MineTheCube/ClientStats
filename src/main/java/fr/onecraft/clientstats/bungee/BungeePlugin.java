aaf3110829772857b67347f9022ee75252d26c3b
MineTheCube <playminethecube@gmail.com>
2016-08-20 02:13:06 +0200

    Add BungeeYAML dependency

--- src/main/java/fr/onecraft/clientstats/bungee/BungeePlugin.java
+++ src/main/java/fr/onecraft/clientstats/bungee/BungeePlugin.java
@@ -1,14 +1,15 @@
 package fr.onecraft.clientstats.bungee;
 
 import fr.onecraft.clientstats.ClientStats;
-import fr.onecraft.clientstats.bungee.config.PluginConfigurable;
 import fr.onecraft.clientstats.bungee.dispatcher.CommandDispatcher;
 import fr.onecraft.clientstats.bungee.dispatcher.EventDispatcher;
 import fr.onecraft.clientstats.bungee.user.BungeeUserProvider;
+import fr.onecraft.clientstats.common.base.Configurable;
 import fr.onecraft.clientstats.common.core.AbstractAPI;
 import fr.onecraft.clientstats.common.user.MixedUser;
+import fr.onecraft.config.PluginConfigurable;
 
-public class BungeePlugin extends PluginConfigurable {
+public class BungeePlugin extends PluginConfigurable implements Configurable {
 
     // Plugin state
     private boolean enabled = false;
@@ -46,6 +47,27 @@ public class BungeePlugin extends PluginConfigurable {
 
     public boolean isEnabled() {
         return enabled;
+    }
+
+    @Override
+    public void migrate() {}
+
+    @Override
+    public void options() {
+
+        // Copy headers and new values
+        getConfig().options().copyDefaults(true).copyHeader(true);
+
+    }
+
+    @Override
+    public String getConfigString(String path) {
+        return getConfig().getString(path);
+    }
+
+    @Override
+    public String getConfigString(String path, String def) {
+        return getConfig().getString(path, def);
     }
 
 }
